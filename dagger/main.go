1d7b331d5fef3572e8355f79bdedf92ff68676d3
Jeremy Adams <jeremy@dagger.io>
2024-05-06 20:42:38 -0700

    simplify
    Signed-off-by: Jeremy Adams <jeremy@dagger.io>

--- dagger/main.go
+++ dagger/main.go
@@ -1,16 +1,4 @@
-// A generated module for Multi functions
-//
-// This module has been generated via dagger init and serves as a reference to
-// basic module structure as you get started with Dagger.
-//
-// Two functions have been pre-created. You can modify, delete, or add to them,
-// as needed. They demonstrate usage of arguments and return types using simple
-// echo and grep commands. The functions can be called from the dagger CLI or
-// from one of the SDKs.
-//
-// The first line in this comment block is a short description line and the
-// rest is a long description with more detail on the module's purpose or usage,
-// if appropriate. All modules should have a short description.
+// An example for building single platform and multi platform images
 
 package main
 
@@ -29,7 +17,7 @@ func (m *Multi) BuildPush(ctx context.Context, platform string, image string, ta
 		Publish(ctx, fmt.Sprintf("ttl.sh/%v-%v:%v", image, platform, tag))
 }
 
-// Push a multi-arch image
+// Push a multi-platform image
 func (m *Multi) MultiPush(ctx context.Context, image string, platforms []string, tag string) (string, error) {
 	ctrs := []*Container{} //empty slice of Containers
 	for _, platform := range platforms {
